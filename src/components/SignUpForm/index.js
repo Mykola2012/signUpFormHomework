91d79ec7bb771e34ea11e14feda38488b09ed4e7
Revika Mukola <revikanikolaj@gmail.com>
2022-04-04 11:13:32 +0200

    Add form

---
+++ src/components/SignUpForm/index.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import styles from "./SignUpForm.module.css";
+
+class SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+    };
+  }
+
+  render() {
+    const { name, email, password, passwordConfirmation } = this.state;
+
+    return (
+      <form className={styles.formContainer}>
+        <lable className={styles.lableFormSign}>
+          <span className={styles.spanFormSign}>Name</span>
+          <input
+            className={styles.inputFormSign}
+            value={name}
+            type="text"
+            name="name"
+            placeholder="name"
+            autoFocus
+          />
+        </lable>
+
+        <lable className={styles.lableFormSign}>
+          <span className={styles.spanFormSign}>Email</span>
+          <input
+            className={styles.inputFormSign}
+            value={email}
+            type="email"
+            name="email"
+            placeholder="email"
+          />
+        </lable>
+
+        <lable className={styles.lableFormSign}>
+          <span className={styles.spanFormSign}>Password</span>
+          <input
+            className={styles.inputFormSign}
+            value={password}
+            type="password"
+            name="password"
+            placeholder="password"
+          />
+        </lable>
+
+        <lable className={styles.lableFormSign}>
+          <span className={styles.spanFormSign}>Password confirmation</span>
+          <input
+            className={styles.inputFormSign}
+            value={passwordConfirmation}
+            type="password"
+            name="passwordConfirmation"
+            placeholder="password confirmation"
+          />
+        </lable>
+      </form>
+    );
+  }
+}
+
+export default SignUpForm;
